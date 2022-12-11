414f97086bbe715454558ca4b830c13c09db537e
konung-yaropolk <yaropolk1995@gmail.com>
2022-12-10 23:13:54 -0600

    .

--- stackreg.py
+++ stackreg.py
@@ -1,40 +1,43 @@
 #!/usr/bin/env python3
-from pystackreg import StackReg
-from skimage import io
+import pystackreg
+import skimage
 import numpy as np
 
-QUEUE = [   
+QUEUE = [
 
-# list here tif file names without extensions, divided py comma
+    # list here TIFF file names without extensions, divided py comma:
 
-'A_0010',    
-'A_0011',
-'A_0012',
-'A_0013',
+    'A_0010',
+    'A_0011',
+    'A_0012',
+    'A_0013',
 
 ]
 
-
 for file in QUEUE:
 
+    try:
+        img = skimage.io.imread(file + '.tif')
+    except:
 
-    img = io.imread(file+'.tif') # 3 dimensions : frames x width x height
+        try:
+            img = skimage.io.imread(file + '.tiff')
+        except:
+            print('\nFile', file, 'not found')
+            continue
 
+    sr = pystackreg.StackReg(
+        pystackreg.StackReg.TRANSLATION)  # TRANSLATION, RIGID_BODY, SCALED_ROTATION, AFFINE, BILINEAR
 
-    sr = StackReg(StackReg.TRANSLATION)  # TRANSLATION, RIGID_BODY, SCALED_ROTATION, AFFINE, BILINEAR
-
-    for ch in range(len(img)):        
-
+    for ch in range(len(img)):
         # register to mean of first 10 images
         out = sr.register_transform_stack(img[ch], reference='first', n_frames=10, verbose=True)
         out = out.astype(np.int16)
-        io.imsave('{}_ch{}.tif'.format(file, ch+1), out)
+        skimage.io.imsave('{}_ch{}.tif'.format(file, ch + 1), out)
 
-    print('\n',file,'done!\n\n')
+    print('\n', file, 'done!\n\n')
 
-print('Series done!\n')
-
-
+print('\nSeries done!\n')
 
 # register each frame to the previous (already registered) one
 # this is what the original StackReg ImageJ plugin uses
@@ -50,5 +53,3 @@ print('Series done!\n')
 # calculate a moving average of 10 images, then register the moving average to the mean of
 # the first 10 images and transform the original image (not the moving average)
 # out_moving10 = sr.register_transform_stack(img, reference='first', n_frames=10, moving_average = 10)
-
-
