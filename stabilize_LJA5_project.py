3bd46778cff23f054f63db0a283c7857535b1c8a
Yaroslav Andrianov <yaropolk1995@gmail.com>
2025-08-28 22:17:45 -0500

    modified:   stabilize_LJA5_project.py

--- stabilize_LJA5_project.py
+++ stabilize_LJA5_project.py
@@ -92,22 +92,22 @@ TODO_LIST = [
     ['Field_1_Dynorphin_application',
         {
             'DIRECTORY': 'F:/Lab Work Files/Stacey/LJA5 project Dynorphin control/2025_08_25/',
-            'DISTORTION_TYPE': 'BILINEAR',
+            'DISTORTION_TYPE': 'AFFINE',
             'REFERENCE_FRAME': 'first',
             'NUMBER_OF_REF_FRAMES': 20,
             'MOVING_AVERAGE': 20,
         }
      ],
 
-    ['Field_1',
-        {
-            'DIRECTORY': 'F:/Lab Work Files/Stacey/LJA5 project Dynorphin control/2025_08_26/',
-            'DISTORTION_TYPE': 'BILINEAR',
-            'REFERENCE_FRAME': 'first',
-            'NUMBER_OF_REF_FRAMES': 20,
-            'MOVING_AVERAGE': 20,
-        }
-     ],
+    # ['Field_1',
+    #     {
+    #         'DIRECTORY': 'F:/Lab Work Files/Stacey/LJA5 project Dynorphin control/2025_08_26/',
+    #         'DISTORTION_TYPE': 'BILINEAR',
+    #         'REFERENCE_FRAME': 'first',
+    #         'NUMBER_OF_REF_FRAMES': 20,
+    #         'MOVING_AVERAGE': 20,
+    #     }
+    #  ],
 
 
 
